311a0da09a096d448cb1e5efe5ac5ffe1194457b
Naseemtaha <46400979+Naseemtaha@users.noreply.github.com>
2019-01-06 20:08:05 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
  
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("ا!play",{type: 'WATCHING'})
+   client.user.setActivity("Type!play",{type: 'WATCHING'})
   console.log('')
   console.log('')
   console.log('╔[═════════════════════════════════════════════════════════════════]╗')
